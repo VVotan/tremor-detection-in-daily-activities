91e1d0c3e75ca48dc37739f0fd02b17a2a3aa4bf
Odin <ucxuj@student.kit.edu>
2026-06-13 03:47:24 +0200

    add PyYAML in requirements.txt

--- src/wavelet_analysis.py
+++ src/wavelet_analysis.py
@@ -1,6 +1,7 @@
 """Wavelet-based time-frequency analysis for IMU tremor signals."""
 import numpy as np
 from scipy.signal import welch
+from src.visualization import VisualizationConfig, Visualizer
 
 def start_wavelet_analysis(data, min_frequency, max_frequency, sampling_rate):
     pass
